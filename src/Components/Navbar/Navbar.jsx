1a0dac66534d6ad4c0a911c82bc90ec6dab765b0
Maruf Hasan Shihab <marufhasanshihab2@gmail.com>
2024-08-14 15:32:45 +0600

    first commit

---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,10 @@
+import logo from "../../assets/Images/logo.png"
+const Navbar = () => {
+  return (
+    <div>
+      <img src={logo} alt="logo" className="w-[80px] md:w-[115px] object-cover" />
+    </div>
+  );
+};
+
+export default Navbar;
